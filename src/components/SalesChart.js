66061268d656ca966b2d228a9d31a1514f767b5c
Ashish Meshram <ashish.meshram@tcs.com>
2022-11-20 03:09:15 -0500

    Removed line chart

--- src/components/SalesChart.js
+++ src/components/SalesChart.js
@@ -1,105 +1,73 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
+
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-import { Line } from "react-chartjs-2";
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-export const options2 = {
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
-  };
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [4,5,5,6,6,7,8],
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: [3,4,4,5,5,6,7],
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
-//Chart.defaults.scale.gridLines.drawOnChartArea = false;
 
 const options = {
-      type: 'line',      
-      showScale: false,
-      data: {
-        labels: ['2018', '2019', '2020', '2021', '2022'],
-  
-        datasets: [{
-          label: 'Revenue',
-          showScale: false,
-          fill: false,
-          data: ["5000", "8000", "10000", "14000", "17000"],
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
+  type: "line",
+  showScale: false,
+  data: {
+    labels: ["2018", "2019", "2020", "2021", "2022"],
+
+    datasets: [
+      {
+        label: "Revenue",
+        showScale: false,
+        fill: false,
+        data: ["5000", "8000", "10000", "14000", "17000"],
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        stack: 'Stack 0',
+        barThickness:20
+      }      
+    ]
+  },
+    scales: {
+        x: {
+          grid: {
+            display: false
+          }
         },
-        {
-          label:'Profit',
-          showScale: false,
-          fill: false,
-          showScale: false,
-          data: ["1000", "2000", "3000", "4000", "5000"]
-        }]
-      },
-    }
+        y: {
+          grid: {
+            display: false
+          }
+        }
+      }
+};
 
-
-export default function SalesChart() {          
-        
-          // You can render any custom fallback UI
-          return (
-            <div>
-                <Line options={options2} data={data} />
-                <Line
-                    data={options.data}
-                    options={{
-                    plugins: {
-                        title: {
-                        display: true,
-                        text: "Users Gained between 2016-2020"
-                        },
-                        legend: {
-                        display: false
-                        }
-                    }
-                    }}
-                />
-            </div>
-        );
-    
-    
+export default function SalesChart() {
+  return (
+    <Box
+      sx={{
+        width: 300,              
+      }}
+    >
+        <Bar options={options} data={options.data} />
+   </Box>
+  );
 }
