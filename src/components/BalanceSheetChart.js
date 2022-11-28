112034e944334e285acefcc7420d86642d3bacd0
Ashish Meshram <ashish.meshram@tcs.com>
2022-11-27 23:23:49 -0500

    Trying to draw asset graph manually

--- src/components/BalanceSheetChart.js
+++ src/components/BalanceSheetChart.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import { useRecoilValue } from "recoil";
-import { convertMillions,toSentenceCase } from "../utils.js";
-import {Card, CardContent,CardHeader, Box } from "@mui/material";
+import { convertMillions, toSentenceCase } from "../utils.js";
+import { Card, CardContent, CardHeader, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import {
@@ -43,132 +43,161 @@ const Content = styled(CardContent)(({ theme }) => ({
 
 const TitleStyles = {
   fontSize: "0.7rem",
-   color:"orange" 
-}
+  color: "orange",
+};
 
 const subTitleStyles = {
   fontSize: "0.6rem",
-   color:"grey" 
-}
+  color: "grey",
+};
 
+const assetColors = ["#0A758F", "#0D98BA", "#10BBE5", "#32CBF1", "#5DD6F4"];
 
-const assetColors = [  
-  '#0A758F',
-  '#0D98BA',
-  '#10BBE5',
-  '#32CBF1',
-  '#5DD6F4'
-]
-
-
-const liabilitiesColors = [  
-  '#7d3c34',
-  '#984940',
-  '#b4564b',
-  '#bf7067',
-  '#d7a49e'
-]
+const liabilitiesColors = [
+  "#7d3c34",
+  "#984940",
+  "#b4564b",
+  "#bf7067",
+  "#d7a49e",
+];
 
 export const options = {
   plugins: {
     title: {
-      display: false
+      display: false,
     },
     legend: {
-      display: false   
-    }    
+      display: false,
+    },
   },
   responsive: true,
   maintainAspectRatio: false,
   scales: {
     x: {
-      stacked: true
+      stacked: true,
     },
     y: {
       stacked: true,
       grid: {
-        display: false
-      }
+        display: false,
+      },
     },
   },
 };
 
 let libOptions = JSON.parse(JSON.stringify(options));
 libOptions.scales.y.ticks = {
-  display: false
+  display: false,
 };
 
 export default function BalanceSheetChart() {
-
   const balanceSheet = useRecoilValue(balanceSheetState);
   const latest = balanceSheet.annualReports[0];
 
   const assetsDataset = [];
   const liabilitiesDataset = [];
-  
-  const assets = (({ totalCurrentAssets , intangibleAssets, investments, otherCurrentAssets,propertyPlantEquipment }) => 
-    ({ totalCurrentAssets , intangibleAssets, investments, otherCurrentAssets,propertyPlantEquipment }))(latest);
 
+  const assets = (({
+    totalCurrentAssets,
+    intangibleAssets,
+    investments,
+    otherCurrentAssets,
+    propertyPlantEquipment,
+  }) => ({
+    totalCurrentAssets,
+    intangibleAssets,
+    investments,
+    otherCurrentAssets,
+    propertyPlantEquipment,
+  }))(latest);
 
-  const liabilities = (({ totalCurrentLiabilities , longTermDebt, totalShareholderEquity, otherCurrentLiabilities }) => 
-    ({ totalCurrentLiabilities , longTermDebt, totalShareholderEquity, otherCurrentLiabilities }))(latest);
+  const liabilities = (({
+    totalCurrentLiabilities,
+    longTermDebt,
+    totalShareholderEquity,
+    otherCurrentLiabilities,
+  }) => ({
+    totalCurrentLiabilities,
+    longTermDebt,
+    totalShareholderEquity,
+    otherCurrentLiabilities,
+  }))(latest);
 
-  Object.keys(assets).forEach((e,i) => assetsDataset.push(
-    {
+  Object.keys(assets).forEach((e, i) =>
+    assetsDataset.push({
       label: toSentenceCase(e),
       data: [convertMillions(assets[e])],
-      backgroundColor: assetColors[i]
-    }
-  ));
+      backgroundColor: assetColors[i],
+    })
+  );
 
-  Object.keys(liabilities).forEach((e,i) => liabilitiesDataset.push(
-    {
+  Object.keys(liabilities).forEach((e, i) =>
+    liabilitiesDataset.push({
       label: toSentenceCase(e),
       data: [convertMillions(liabilities[e])],
-      backgroundColor: liabilitiesColors[i]
-    }
-  ));
+      backgroundColor: liabilitiesColors[i],
+    })
+  );
 
   const assetsData = {
-    labels: ['Assets'],
-    datasets: assetsDataset    
+    labels: ["Assets"],
+    datasets: assetsDataset,
   };
 
   const liabilitiesData = {
-    labels: ['Liabilities'],
-    datasets: liabilitiesDataset    
+    labels: ["Liabilities"],
+    datasets: liabilitiesDataset,
   };
 
-  const getAssetsChartHeight = (val) => {    
-    return val * 100 / convertMillions(latest.totalAssets);
+  const getAssetsChartHeight = (val) => {
+    return (val * 100) / convertMillions(latest.totalAssets);
+  };
+
+  const graphItemStyle = (item, idx) => {
+    return {
+      backgroundColor: liabilitiesColors[idx],
+      height: getAssetsChartHeight(item.data) + "px",
+      fontSize: "1rem",
+      color: "#E2DFD2",
+      width: "100px",
+      textAlign: "center",
+      borderBottom: "1px solid #EDEADE",
+    };
   };
 
   return (
     <Card variant="outlined" raised="true">
-        <Header
-          title="BALANCE SHEET"
-          titleTypographyProps={TitleStyles}
-          subheader="As of Date, $million"
-          subheaderTypographyProps={subTitleStyles}
-        ></Header>
-        <Content>
-      <Grid container spacing={2} columns={16} sx={{height:'21rem'}} display="false">
-        <Grid item xs={8}>
-          <Bar options={options} data={assetsData} />
-        </Grid>
-        <Grid item xs={8}>
-        <Grid container spacing={1}>
-            {assetsDataset.map((itemp, index) => (
-              <Grid key={index} sx={{backgroundColor:liabilitiesColors[index], padding:'5px', fontSize: "0.7rem", width: '100px', height: getAssetsChartHeight(itemp.data) + 'px'}}>
-                { getAssetsChartHeight(itemp.data) > 5 ? itemp.data : '' }
-              </Grid>
-            ))}
-        
+      <Header
+        title="BALANCE SHEET"
+        titleTypographyProps={TitleStyles}
+        subheader="As of Date, $million"
+        subheaderTypographyProps={subTitleStyles}
+      ></Header>
+      <Content>
+        <Grid
+          container
+          spacing={2}
+          columns={16}
+          sx={{ height: "21rem" }}
+          display="false"
+        >
+          <Grid item xs={8}></Grid>
+          <Grid item xs={8}>
+            <Grid container spacing={1}>
+              {assetsDataset.map((item, index) => (
+                <Grid key={index} sx={graphItemStyle(item, index)}>
+                  <span>
+                    {getAssetsChartHeight(item.data) > 5 ? item.data : ""}
+                  </span>
+                  <span className="lables">
+                    {getAssetsChartHeight(item.data) > 5 ? item.label : ""}
+                  </span>
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
         </Grid>
-    </Grid>
-
-    </Content>
-      </Card>      
+      </Content>
+    </Card>
   );
 }
